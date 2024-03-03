7689b1c696235c7c825a3d47c53a4d6165f3a879
dylanzu <54517167+dylanzu@users.noreply.github.com>
2024-03-02 20:09:36 -0500

    Update url.js

--- url.js
+++ url.js
@@ -1,6 +1,6 @@
 const ASSETS_URL = "https://raw.githubusercontent.com/mroyale/assets/legacy/";
 const ASSETS_SKIN_URL = "https://raw.githubusercontent.com/mroyale/assets/legacy/img/skins/smb_skin";
-const ASSETS_LOBBY_URL = "https://raw.githubusercontent.com/dylanzu/things/main/";
+const ASSETS_LOBBY_URL = "https://raw.githubusercontent.com/dylanzu/mroyale-lmr/main/";
 
 let LOBBY_MUSIC_URL = ASSETS_LOBBY_URL + "audio/music/lobby" + Math.floor(Math.random() * 8) + ".mp3";
 let MENU_MUSIC_URL = ASSETS_URL + "audio/music/menu.mp3";
